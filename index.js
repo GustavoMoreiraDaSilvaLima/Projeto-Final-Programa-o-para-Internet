628840af46af24605e66c7adf4164e5777b2baec
GustavoMoreiraDaSilvaLima <gustavomoreira3108@hotmail.com>
2023-12-08 22:10:42 -0300

    feats: pagina de erro de login atualizada

--- index.js
+++ index.js
@@ -137,20 +137,29 @@ function ValidarLogin(requisicao, resposta) {
       }
     }
   resposta.end(`
-        <!DOCTYPE html>
-    <html lang="pt-br">
-
-    <head>
-        <meta charset="UTF-8">
-        <link rel="icon" href="/erro.png">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Falha na Autenticação</title>
-    </head>
-    <body>
-      <h3>Usuario ou Senha Invalidos</h3>
-      <a href = "/login.html">Voltar ao Login <a>
-      </body>
-    </html>
+  <!DOCTYPE html>
+  <html lang="pt-br">
+  
+  <head>
+      <meta charset="UTF-8">
+      <link rel="icon" href="/erro.png">
+      <link rel="stylesheet" href="/paginaErro.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <title>Falha na Autenticação</title>
+  </head>
+  
+  <body class="escuro">
+      <div class="altinha">
+          <div class="container border border-danger dentro">
+              <h3 class="text-center letraalto">Usuario ou Senha Invalidos</h3>
+              <p class="letraalto text-center"><a class="text-danger" href="/login.html">Voltar ao Login <a></p>
+          </div>
+      </div>
+  </body>
+  
+  </html>
       `);
 }
 
